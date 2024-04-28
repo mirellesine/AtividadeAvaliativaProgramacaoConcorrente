6f0b3de3ba674ebb61ee0825f39bc511c1fec65c
mirellesine <mirellesine@gmail.com>
2024-04-27 22:56:55 -0300

    Update Main.java to generate Stores and Customers

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Models.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Bank bank = new Bank();
+        for (int i = 0; i < 2; i++) {
+            Store.stores.add(createStore(bank));
         }
+        Customer[] customers = new Customer[5];
+        for (int i = 0; i < 5; i++) {
+            customers[i] = new Customer();
+            customers[i].start();
+        }
+        for (Customer customer : customers) {
+            try {
+                customer.join();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        // Print the balances of all stores and their employees.
+        for (int i = 0; i < Store.stores.size(); i++) {
+            Store store = Store.stores.get(i);
+            System.out.printf("Store %d has balance of $%.2f\n", i, store.getAccount().getBalance());
+            for (int j = 0; j < store.getEmployees().length; j++) {
+                Employee employee = store.getEmployees()[j];
+                System.out.printf("Employee %d has a balance of $%.2f in his salary account\n", j, employee.getSalaryAccount().getBalance());
+                System.out.printf("Employee %d has a balance of $%.2f in his investment account\n", j, employee.getInvestmentAccount().getBalance());
+            }
+        }
+        // Print the balances of all customers.
+        for (int i = 0; i < customers.length; i++) {
+            System.out.printf("Customer %d has balance of $%.2f\n", i, customers[i].getAccount().getBalance());
+        }
+    }
+
+    private static Store createStore(Bank bank) {
+        return new Store(new Account(0), bank, createEmployeesForStore());
+    }
+
+    private static Employee[] createEmployeesForStore() {
+        return new Employee[]{createEmployee(), createEmployee()};
+    }
+
+    private static Employee createEmployee() {
+        return new Employee(new Account(0), new Account(0));
     }
 }
